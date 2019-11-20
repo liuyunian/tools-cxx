8e3445d8895a1781805277e216d013a246356dd1
liuyunian <sdjn_lyn@163.com>
2019-11-20 22:04:34 +0800

    feat-examples/config: test parse bool type

--- examples/config/config_test.cpp
+++ examples/config/config_test.cpp
@@ -16,13 +16,16 @@ int main(){
         LOG_FATAL("Failed to load config file: %s", configFileName);
     }
 
-    std::cout << cf.get<std::string>("StringTest") << std::endl;
+    std::cout << cf.get<std::string>("StringTest") << '\n';
 
     int int_val = cf.get<int>("IntTest");
-    std::cout << int_val << std::endl;
+    std::cout << int_val << '\n';
 
     double double_val = cf.get<double>("DoubleTest");
-    std::cout << double_val << std::endl;
+    std::cout << double_val << '\n';
+
+    bool bool_val = cf.get<bool>("BoolTest");
+    std::cout << std::boolalpha << bool_val << std::endl;
 
     return 0;
 }
