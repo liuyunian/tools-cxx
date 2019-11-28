94ac42e23b615edd90f7e8149a8df7a7fdb4100b
liuyunian <sdjn_lyn@163.com>
2019-11-28 16:29:52 +0800

    refactor-log/log: static functions

--- tools/log/log.cpp
+++ tools/log/log.cpp
@@ -13,78 +13,73 @@ Log::OutputFunc Log::m_output = default_output;
 Log::FlushFunc Log::m_flush = default_flush;
 
 Log::Log(LogLevel logLevel) : 
-    m_curLevel(logLevel),
-    m_errnoSave(0){}
+  m_curLevel(logLevel),
+  m_errnoSave(0){}
 
 Log::Log(bool toAbort) : 
-    m_curLevel(toAbort?FATAL:ERROR),
-    m_errnoSave(errno){}
+  m_curLevel(toAbort?FATAL:ERROR),
+  m_errnoSave(errno){}
 
 Log::~Log(){
-    const LogBuffer& buffer(m_print.get_buffer());
-    m_output(buffer.data(), buffer.length());
+  const LogBuffer& buffer(m_print.get_buffer());
+  m_output(buffer.data(), buffer.length());
 
-    if(m_curLevel == FATAL){
-        m_flush();
-        abort();
-    }
+  if(m_curLevel == FATAL){
+    m_flush();
+    abort();
+  }
 }
 
 const char* LogLevelName[Log::NUM_LOG_LEVELS] = {
-    "[DEBUG]",
-    "[INFO]",
-    "[WARN]",
-    "[ERROR]",
-    "[FATAL]",
+  "[DEBUG]",
+  "[INFO]",
+  "[WARN]",
+  "[ERROR]",
+  "[FATAL]",
 };
 
 void Log::log(const char *file, int line, const char *fmt, ...){
-    Timestamp now(Timestamp::now());
-    m_print.printf("%s %s %s ", now.to_formatted_string().c_str(), CurrentThread::get_tid_string(), LogLevelName[m_curLevel]);  // 时间戳 线程ID 日志级别
-    
-    va_list args;
-    va_start(args, fmt);
-    m_print.vprintf(fmt, args);                                                                                                 // 正文
-    va_end(args);
+  Timestamp now(Timestamp::now());
+  m_print.printf("%s %s %s ", 
+                  now.to_formatted_string().c_str(),    // 时间戳
+                  CurrentThread::get_tid_string(),      // 线程ID
+                  LogLevelName[m_curLevel]              // 日志级别
+                );                                      
+  
+  va_list args;
+  va_start(args, fmt);
+  m_print.vprintf(fmt, args);                           // 正文
+  va_end(args);
 
-    if(m_errnoSave != 0){
-        m_print.printf(" %s (errno = %d)", m_errnoSave);                                                                        // strerrno (errno = ?)
-    }
+  if(m_errnoSave != 0){
+    m_print.printf(" %s (errno = %d)", m_errnoSave);    // strerrno (errno = ?)
+  }
 
-    m_print.printf(" - %s: %d\n", ::basename(const_cast<char*>(file)), line);                                                   // - 所在文件名: 行号
+  m_print.printf(" - %s: %d\n", 
+                  ::basename(const_cast<char*>(file)),  // 所在文件名
+                  line
+                );                                      // 行号
 }
 
 Log::LogLevel Log::init_threshold_level(){
-    if (::getenv("LOG_DEBUG")){                         // 如果定义了环境变量LOG_DEBUG，日志级别设置为DEBUG
-        return DEBUG;
-    }
-    else if(::getenv("LOG_WARN")){
-        return WARN;
-    }                                                                  
-    else{
-        return INFO;
-    }
-}
-
-void Log::set_level(LogLevel level){
-    m_thresholdLevel = level;
-}
-
-void Log::set_output(OutputFunc output){
-    m_output = output;
+  if (::getenv("LOG_DEBUG")){                           // 如果定义了环境变量LOG_DEBUG，日志级别设置为DEBUG
+    return DEBUG;
+  }
+  else if(::getenv("LOG_WARN")){                        // 日志级别设置为WARN
+    return WARN;
+  }                                                                  
+  else{
+    return INFO;
+  }
 }
 
 void Log::default_output(const char* msg, int len){
-    size_t n = fwrite(msg, 1, len, stdout);
-    if(n < 0){
-        perror("Failed to call fwrite int Log::default_output");
-    }
-}
-
-void Log::set_flush(FlushFunc flush){
-    m_flush = flush;
+  size_t n = fwrite(msg, 1, len, stdout);
+  if(n < 0){
+    perror("Failed to call fwrite int Log::default_output");
+  }
 }
 
 void Log::default_flush(){
-    fflush(stdout);
+  fflush(stdout);
 }
--- tools/log/log.h
+++ tools/log/log.h
@@ -10,55 +10,59 @@
 
 class Log : noncopyable {
 public:
-    enum LogLevel {
-        DEBUG,          // 用于调试
-        INFO,           // 提示
-        WARN,           // 警告
-        ERROR,          // 错误
-        FATAL,          // 致命错误
-        NUM_LOG_LEVELS  // 日志级别数
-    };
+  enum LogLevel {
+    DEBUG,          // 用于调试
+    INFO,           // 提示
+    WARN,           // 警告
+    ERROR,          // 错误
+    FATAL,          // 致命错误
+    NUM_LOG_LEVELS  // 日志级别数
+  };
 
-    explicit Log(LogLevel logLevel);
-    explicit Log(bool toAbort);
-    ~Log();
+  explicit Log(LogLevel logLevel);
+  explicit Log(bool toAbort);
+  ~Log();
 
-    void log(const char *file, int line, const char* fmt, ...);
+  void log(const char *file, int line, const char* fmt, ...);
 
-    static LogLevel get_threshold_level();
-    static LogLevel init_threshold_level();
-    static void set_level(LogLevel level);
+  static LogLevel init_threshold_level();
+  static LogLevel get_threshold_level(){
+    return m_thresholdLevel;
+  }
+  static void set_level(LogLevel level){
+    m_thresholdLevel = level;
+  }
 
-    typedef std::function<void(const char* msg, int len)> OutputFunc;
-    static void set_output(OutputFunc output);
-    static void default_output(const char* msg, int len);
+  typedef std::function<void(const char*, int)> OutputFunc;
+  static void default_output(const char *msg, int len);
+  static void set_output(const OutputFunc &output){
+    m_output = output;
+  }
 
-    typedef std::function<void()> FlushFunc;
-    static void set_flush(FlushFunc flush);
-    static void default_flush();
+  typedef std::function<void()> FlushFunc;
+  static void default_flush();
+  static void set_flush(const FlushFunc &flush){
+    m_flush = flush;
+  }
 
 private:
-    static LogLevel m_thresholdLevel;
-    static OutputFunc m_output;
-    static FlushFunc m_flush;
+  static LogLevel m_thresholdLevel;
+  static OutputFunc m_output;
+  static FlushFunc m_flush;
 
-    LogLevel m_curLevel;
-    int m_errnoSave;
-    LogPrint m_print;
+  LogLevel m_curLevel;
+  int m_errnoSave;
+  LogPrint m_print;
 };
 
-inline Log::LogLevel Log::get_threshold_level(){  // 内联函数
-    return m_thresholdLevel;
-}
-
 #define LOG_DEBUG(fmt, ...) if(Log::get_threshold_level() <= Log::DEBUG) \
-    Log(Log::DEBUG).log(__FILE__, __LINE__, fmt, ##__VA_ARGS__)
+  Log(Log::DEBUG).log(__FILE__, __LINE__, fmt, ##__VA_ARGS__)
 
 #define LOG_INFO(fmt, ...) if(Log::get_threshold_level() <= Log::INFO) \
-    Log(Log::INFO).log(__FILE__, __LINE__, fmt, ##__VA_ARGS__)
+  Log(Log::INFO).log(__FILE__, __LINE__, fmt, ##__VA_ARGS__)
 
 #define LOG_WARN(fmt, ...) if(Log::get_threshold_level() <= Log::WARN) \
-    Log(Log::WARN).log(__FILE__, __LINE__, fmt, ##__VA_ARGS__)
+  Log(Log::WARN).log(__FILE__, __LINE__, fmt, ##__VA_ARGS__)
 
 #define LOG_ERR(fmt, ...) Log(Log::ERROR).log(__FILE__, __LINE__, fmt, ##__VA_ARGS__)
 
