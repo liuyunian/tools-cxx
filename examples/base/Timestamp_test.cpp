85a53b8bf55b5b5e2470b705c042757a44a0bbb8
liuyunian <sdjn_lyn@163.com>
2019-12-25 12:03:20 +0800

    feat-base/Timestamp: add get_difference

--- examples/base/Timestamp_test.cpp
+++ examples/base/Timestamp_test.cpp
@@ -2,27 +2,31 @@
 #include <tools/base/Timestamp.h>
 
 int main(){
-    Timestamp now = Timestamp::now();                                                // test static now()
+  Timestamp now = Timestamp::now();                                               // test static now()
 
-    std::cout << "microseconds = " << now.get_microseconds_since_epoch() << '\n';   // test get_microseconds_since_epoch()
-    std::cout << "seconds = " << now.get_seconds_since_epoch() << '\n';             // test get_seconds_since_epoch()
-    std::cout << "period = " << now.get_period() << '\n';                           // test get_period
+  std::cout << "microseconds = " << now.get_microseconds_since_epoch() << '\n';   // test get_microseconds_since_epoch()
+  std::cout << "seconds = " << now.get_seconds_since_epoch() << '\n';             // test get_seconds_since_epoch()
+  std::cout << "period = " << now.get_period() << '\n';                           // test get_period
 
-    std::cout << now.to_string() << '\n';                                           // test to_string
-    std::cout << now.to_formatted_string() << '\n';                                 // test to_formatted_string
-    std::cout << now.to_formatted_string(false) << '\n';
+  std::cout << now.to_string() << '\n';                                           // test to_string
+  std::cout << now.to_formatted_string() << '\n';                                 // test to_formatted_string
+  std::cout << now.to_formatted_string(false) << '\n';
 
-    std::cout << now.add_time(2).to_formatted_string() << std::endl;
+  std::cout << now.add_time(2).to_formatted_string() << std::endl;                // test add_time
 
-    Timestamp invalidTimestamp = Timestamp::invalid();                              // test static invalid()
+  Timestamp when = now;
+  now = Timestamp::now();
+  std::cout << "diff = " << now.get_difference(when) << std::endl;                // test get_difference
 
-    std::cout << std::boolalpha << (now == invalidTimestamp) << '\n';               // test = !=
-    std::cout << std::boolalpha << (now != invalidTimestamp) << '\n';
+  Timestamp invalidTimestamp = Timestamp::invalid();                              // test static invalid()
 
-    std::cout << std::boolalpha << (now < invalidTimestamp) << '\n';                // test < > <= >=
-    std::cout << std::boolalpha << (now > invalidTimestamp) << '\n';
-    std::cout << std::boolalpha << (now <= invalidTimestamp) << '\n';
-    std::cout << std::boolalpha << (now >= invalidTimestamp) << '\n';
+  std::cout << std::boolalpha << (now == invalidTimestamp) << '\n';               // test = !=
+  std::cout << std::boolalpha << (now != invalidTimestamp) << '\n';
 
-    return 0;
+  std::cout << std::boolalpha << (now < invalidTimestamp) << '\n';                // test < > <= >=
+  std::cout << std::boolalpha << (now > invalidTimestamp) << '\n';
+  std::cout << std::boolalpha << (now <= invalidTimestamp) << '\n';
+  std::cout << std::boolalpha << (now >= invalidTimestamp) << '\n';
+
+  return 0;
 }
--- tools/base/Timestamp.h
+++ tools/base/Timestamp.h
@@ -15,59 +15,63 @@ class Timestamp : copyable,
                   public boost::less_than_comparable<Timestamp> // 继承boost::less_than_comparable，要求实现<操作符函数，并自动提供'>' '<=' '>='操作符函数
 {
 public:
-    Timestamp() : 
-        m_microsecondsSinceEpoch(0){}
+  Timestamp() : 
+    m_microsecondsSinceEpoch(0){}
 
-    explicit Timestamp(int64_t microseconds) : 
-        m_microsecondsSinceEpoch(microseconds){}
+  explicit Timestamp(int64_t microseconds) : 
+    m_microsecondsSinceEpoch(microseconds){}
 
-    ~Timestamp() = default;
+  ~Timestamp() = default;
 
-    bool is_valid(){
-        return m_microsecondsSinceEpoch > 0;
-    }
+  bool is_valid(){
+    return m_microsecondsSinceEpoch > 0;
+  }
 
-    int64_t get_microseconds_since_epoch() const {
-        return m_microsecondsSinceEpoch;
-    }
+  int64_t get_microseconds_since_epoch() const {
+    return m_microsecondsSinceEpoch;
+  }
 
-    int64_t get_seconds_since_epoch() const {
-        return m_microsecondsSinceEpoch / kmicrosecondsPerSecond;
-    }
+  int64_t get_seconds_since_epoch() const {
+    return m_microsecondsSinceEpoch / kmicrosecondsPerSecond;
+  }
 
-    /**
-     * @brief 以86400s为单位作为一个period，比如0-86399属于一个period
-    */
-    int64_t get_period() const {
-        return m_microsecondsSinceEpoch / kmicrosecondsPerSecond / ksecondsPerDay;
-    }
+  /**
+   * @brief 以86400s为单位作为一个period，比如0-86399属于一个period
+  */
+  int64_t get_period() const {
+    return m_microsecondsSinceEpoch / kmicrosecondsPerSecond / ksecondsPerDay;
+  }
 
-    std::string to_string() const;
-    std::string to_formatted_string(bool showMicroseconds = true) const;
+  std::string to_string() const;
+  std::string to_formatted_string(bool showMicroseconds = true) const;
 
-    /**
-     * @brief timestamp + second
-     * @return 返回增加之后的时间戳
-    */
-    Timestamp add_time(double second){
-        int64_t delta = static_cast<int64_t>(second * kmicrosecondsPerSecond);
-        return Timestamp(m_microsecondsSinceEpoch + delta);
-    }
+  /**
+   * @brief timestamp + second
+   * @return 返回增加之后的时间戳
+  */
+  Timestamp add_time(double second){
+    int64_t delta = static_cast<int64_t>(second * kmicrosecondsPerSecond);
+    return Timestamp(m_microsecondsSinceEpoch + delta);
+  }
 
-    /**
-     * @brief 产生无效的时间戳
-    */
-    static Timestamp invalid(){
-        return Timestamp();
-    }
+  int64_t get_difference(Timestamp other) const {
+    return m_microsecondsSinceEpoch - other.get_microseconds_since_epoch();
+  }
 
-    static Timestamp now();
+  /**
+   * @brief 产生无效的时间戳
+  */
+  static Timestamp invalid(){
+    return Timestamp();
+  }
+
+  static Timestamp now();
+
+  static const int kmicrosecondsPerSecond = 1000 * 1000;
+  static const int ksecondsPerDay = 60*60*24;
 
 private:
-    const static int kmicrosecondsPerSecond = 1000 * 1000;
-    const static int ksecondsPerDay = 60*60*24;
-
-    int64_t m_microsecondsSinceEpoch;
+  int64_t m_microsecondsSinceEpoch;
 };
 
 // 作为友元函数重载'=='和'<'运算符
